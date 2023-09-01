78c5a90be6868f507363ad1931f14594cd75cbda
Vivek Singh <129720016+viveksingh1198@users.noreply.github.com>
2023-09-01 20:28:39 +0530

    Add files via upload

---
+++ registeration form/script.js
@@ -0,0 +1,9 @@
+document.getElementById("register-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Handle form submission here
+    // You can access form inputs using document.getElementById()
+});
+
+document.getElementById("google-button").addEventListener("click", function() {
+    // Handle Google login here
+});
